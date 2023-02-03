b29315ca2cdfdf3bb1a8139ce1a10fa3b604fb69
badriakkala <lcb2021029@iiitl.ac.in>
2023-02-03 23:01:26 +0530

    first commit

---
+++ app/src/main/java/com/example/ubuntufirst/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ubuntufirst
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
